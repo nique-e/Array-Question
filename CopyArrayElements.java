2547cd723688c6ff41f3d1005d7b068112f3ed25
nique-e <niteshmule023@gmail.com>
2025-11-02 22:27:13 +0530

    copy array element solved

---
+++ CopyArrayElements.java
@@ -0,0 +1,22 @@
+
+public class CopyArrayElements {
+
+    public static void main(String[] args) {
+
+        int arr1[] = {10, 20, 30, 40, 50};
+        int arr2[] = new int[arr1.length];
+
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = arr1[i];
+
+        }
+        System.out.println("Elements of first array:");
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.println(arr1[i] + " ");
+        }
+        System.out.println("\nElements copied into second array:");
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.println(arr2[i] + " ");
+        }
+    }
+}
