ab97a50fe06b8e2cbfd275fcacf3bc382b94b638
nique-e <niteshmule023@gmail.com>
2025-11-14 22:10:43 +0530

    frequency sol

---
+++ Frequency.java
@@ -0,0 +1,24 @@
+public class Frequency {
+    public static void main(String[] args) {
+
+        int arr[] = {10, 20, 10, 30, 20, 10};
+        boolean visited[] = new boolean[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (visited[i] == true)
+                continue;
+
+            int count = 1;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    visited[j] = true;
+                    count++;
+                }
+            }
+
+            System.out.println(arr[i] + " → " + count + " times");
+        }
+    }
+}
