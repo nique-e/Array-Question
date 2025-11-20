d344acf6972f221f7da6ba4536ad7826f8cc5e90
nique-e <niteshmule023@gmail.com>
2025-11-20 21:42:11 +0530

    count occurrence sol

---
+++ CountOccurrence.java
@@ -0,0 +1,16 @@
+public class CountOccurrence {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 2, 4, 2, 5};
+        int target = 2;
+
+        int count = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == target) {
+                count++;
+            }
+        }
+
+        System.out.println("Occurrences of " + target + ": " + count);
+    }
+}
