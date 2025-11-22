8f5d3390e1cadf3f1417936727115f5db58048f3
nique-e <niteshmule023@gmail.com>
2025-11-22 23:51:29 +0530

    leader element sol

---
+++ LeaderElements .java
@@ -0,0 +1,20 @@
+public class LeaderElements {
+
+    public static void main(String[] args) {
+
+        int[] arr = {16, 17, 4, 3, 5, 2};
+
+        int n = arr.length;
+        int maxFromRight = arr[n - 1];
+
+        System.out.print("Leader elements: " + maxFromRight + " ");
+
+        // Traverse from second last element to first
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] > maxFromRight) {
+                maxFromRight = arr[i];
+                System.out.print(maxFromRight + " ");
+            }
+        }
+    }
+}
