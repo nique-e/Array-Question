ac3a81462a6d34fbabde22fed42d079cd327036f
nique-e <niteshmule023@gmail.com>
2025-11-23 23:11:27 +0530

    longest consecutive sequence sol

---
+++ LongestConsecutiveSequence.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class LongestConsecutiveSequence {
+    public static int longestConsecutive(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) set.add(num);
+
+        int longest = 0;
+
+        for (int num : set) {
+            
+            if (!set.contains(num - 1)) {
+                int current = num;
+                int count = 1;
+
+                
+                while (set.contains(current + 1)) {
+                    current++;
+                    count++;
+                }
+
+                longest = Math.max(longest, count);
+            }
+        }
+
+        return longest;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {100, 4, 200, 1, 3, 2};
+        System.out.println("Longest Consecutive Length: " + longestConsecutive(arr));
+    }
+}
