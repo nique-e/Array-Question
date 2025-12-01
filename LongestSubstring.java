8a5358b438b7d014df0b3f7716cdc2014565d40c
nique-e <niteshmule023@gmail.com>
2025-12-01 22:49:41 +0530

    longest substring sol

---
+++ LongestSubstring.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class LongestSubstring {
+    public static int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int left = 0, maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        System.out.println("Longest substring length: " + lengthOfLongestSubstring(s));
+    }
+}
